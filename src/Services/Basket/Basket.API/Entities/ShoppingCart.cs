4aefbc76acd08bd6d98bfafc095f0bace99903ec
RAHUL26R <rahulroy.profile@gmail.com>
2023-05-27 21:40:42 +0530

    Created Basket.API microservice and configured redis cache

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+            
+        }
+
+        public ShoppingCart(string userName)
+        {
+            Username = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
