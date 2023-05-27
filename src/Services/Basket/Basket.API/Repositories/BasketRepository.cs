3481eb2b93974b6037a53b86411e1881d8125a11
RAHUL26R <rahulroy.profile@gmail.com>
2023-05-27 23:19:33 +0530

    Implemented repository pattern for Basket API

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+           var basket = await _redisCache.GetStringAsync(username);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+           return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            await _redisCache.SetStringAsync(cart.Username, JsonConvert.SerializeObject(cart));
+            return await GetBasket(cart.Username);
+        }
+
+        public async Task DeleteBasket(string username)
+        {
+            await _redisCache.RemoveAsync(username);
+        }        
+    }
+}
