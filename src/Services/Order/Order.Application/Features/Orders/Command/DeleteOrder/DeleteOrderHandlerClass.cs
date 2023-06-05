0e976e66e371e52dddd14e9a7c7a8a219310f39c
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-05 12:38:54 +0530

    Implemented DeleteOrder Command and Exception Handling in Order.Application Layer

--- src/Services/Order/Order.Application/Features/Orders/Command/DeleteOrder/DeleteOrderHandlerClass.cs
+++ src/Services/Order/Order.Application/Features/Orders/Command/DeleteOrder/DeleteOrderHandlerClass.cs
@@ -1,37 +1,35 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using Order.Application.Contracts.Infrastructure;
 using Order.Application.Contracts.Persistence;
 using Order.Application.Exceptions;
-using Order.Application.Models;
 using Order.Domain.Entities;
 
-namespace Order.Application.Features.Orders.Command.UpdateOrder
+namespace Order.Application.Features.Orders.Command.DeleteOrder
 {
-    public class UpdateOrderHandlerClass : IRequestHandler<UpdateOrderCommand>
+    public class DeleteOrderHandlerClass : IRequestHandler<DeleteOrderCommand>
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IMapper _mapper;
-        private readonly ILogger<UpdateOrderHandlerClass> _logger;
+        private readonly ILogger<DeleteOrderHandlerClass> _logger;
 
-        public UpdateOrderHandlerClass(IOrderRepository orderRepository, IMapper mapper,ILogger<UpdateOrderHandlerClass> logger)
+        public DeleteOrderHandlerClass(IOrderRepository orderRepository, IMapper mapper,ILogger<DeleteOrderHandlerClass> logger)
         {
             _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
         {
             var orderEntity = await _orderRepository.GetByIdAsync(request.Id);
             if(orderEntity == null)
             {
                 throw new NotFoundException(nameof(OrderEntity), request.Id);
             }
-            _mapper.Map(request,orderEntity,typeof(UpdateOrderCommand),typeof(OrderEntity));
-            await _orderRepository.UpdateAsync(orderEntity);
-            _logger.LogInformation($"Order with order id {request.Id} is successfully updated");
+            _mapper.Map(request,orderEntity,typeof(DeleteOrderCommand),typeof(OrderEntity));
+            await _orderRepository.DeleteAsync(orderEntity);
+            _logger.LogInformation($"Order with order id {request.Id} is successfully deleted");
             return Unit.Value;
 
         }
