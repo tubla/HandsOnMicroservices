d08f9723cb9a1afbe274a2ff5e530458fdd5889d
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-05 13:22:19 +0530

    Implemented Behaviours in Order.Application layer using CQRS and clean architecture

---
+++ src/Services/Order/Order.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = Order.Application.Exceptions.ValidationException;
+
+namespace Order.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+               var validationContext = new ValidationContext<TRequest>(request);
+               var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(validationContext, cancellationToken)));
+               var failures = validationResult.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                if(failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+            return await next();
+        }
+    }
+}
