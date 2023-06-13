3307a864afc58cdb01289293665b4645eb94aca9
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-13 21:16:59 +0530

    Implemented Aggregate pattern to query over multiple microservices and create a single response containing the combined details of all responses from requested microservices.

---
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,33 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            var response = await _httpClient.GetAsync("/api/v1/catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            var response = await _httpClient.GetAsync($"/api/v1/catalog/{id}");
+            return await response.ReadContentAs<CatalogModel>();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var response = await _httpClient.GetAsync($"/api/v1/catalog/GetCatalogByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+    }
+}
