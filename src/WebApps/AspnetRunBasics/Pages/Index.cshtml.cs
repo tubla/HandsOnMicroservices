f13f4402381d9d54b9b06b6c4eb231ac7acf983d
RAHUL26R <rahulroy.profile@gmail.com>
2023-06-14 11:25:13 +0530

    Created Index and Product page

--- src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspnetRunBasics.Models;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspnetRunBasics.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ICatalogService catalogService, IBasketService basketService)
         {
-            _logger = logger;
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
         }
 
-        public void OnGet()
-        {
+        public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
 
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ProductList = await _catalogService.GetCatalog();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
+        {
+            var product = await _catalogService.GetCatalog(productId);
+            var userName = "swn";
+            var basket = await _basketService.GetBasket(userName);
+            basket.Items.Add(new BasketItemModel() { ProductId = productId, ProductName = product.Name, Price = product.Price, Quantity = 1, Color = "Black" });
+            var updatedBasket = await _basketService.UpdateBasket(basket);
+            return RedirectToPage("Cart");
         }
     }
 }
